d16f4634019f3054b86e2f04f0451a11c501d9b3
lukeparham <lappp9@gmail.com>
2011-09-12 19:12:35 -0500

    Initial Commit

---
+++ helloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloWorld
+//
+//  Created by Luke Parham on 9/12/11.
+//  Copyright Mizzou 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
