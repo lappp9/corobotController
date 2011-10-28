289204183f35c3b49bb9ad871995aa0c39649848
Scott Barry <welivebyfaithnotbysight@gmail.com>
2011-10-28 13:47:59 -0500

    lost accelerometer reading, gotta figure out why

--- www/js/accelerometer.js
+++ www/js/accelerometer.js
@@ -9,9 +9,6 @@ function startWatch() {
 }
 
 // Stop watching the acceleration
-// this is mainly made for a button click or etc.  So onClick='stopWatch()' and it stops tracking accelerometer
-// we discussed a 'kill' switch if the iphone was dropped or something, and it shuts off the communication.
-// this would shut off the movement of the corobot...
 function stopWatch() {
 	if (watchID) {
 		navigator.accelerometer.clearWatch(watchID);
@@ -25,24 +22,25 @@ function stopWatch() {
 // i'm using the json2xml to convert them to xml and displaying them instead of sending them...?
 function onSuccess(acceleration) {
 	
-	//builds JSON and calls the xml converter to return xml into var 'xml'
 	$(function() {
 	  
 	  var x = Math.round(acceleration.x*Math.pow(10,2))/Math.pow(10,2);
 	  var y = Math.round(acceleration.y*Math.pow(10,2))/Math.pow(10,2);
 	  
+	  //making some jsonish shtuff to send to server... 
 	  var drive = {
 			translation:  x,
 			rotation: y
 	  };
 	  
+	  //converts json to xml using jquery
 	  var options = { formatOutput: false };
 	  var xml = $.json2xml(drive, options);
 	  
 	  
 	  //-----NOTE:----- var XML has the xml data.  we are not using it yet, since we don't know what we're doing yet.
 	  
-	  //this is strictly to show modes are changing from drive(accelerometer) to claw.  displays the actual text of acceleration
+	  //displays acceleration to prove it's working...
 	  var element = document.getElementById("tempValueDisplayAccelerometer");
 	  
 	  element.innerHTML = "<br/>" + "Tracking Accelerometer: " +
